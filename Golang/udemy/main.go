afd53e15f15c06b9152f5f580db52f47b04da718
samsul marif <samsul.marif100@gmail.com>
2024-06-10 06:51:56 +0800

    rediver function

--- Golang/udemy/main.go
+++ Golang/udemy/main.go
@@ -1,16 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of diamonds", newCard()}
-	cards = append(cards, "Six of spades")
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Five of diamonds"
+	cards.print()
 }
