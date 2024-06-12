816d6d30a90e838aeecebdd8c2b0e02a3b930eba
samsul marif <samsul.marif100@gmail.com>
2024-06-12 10:08:52 +0800

    random number generation

--- Golang/udemy/deck.go
+++ Golang/udemy/deck.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"math/rand"
+	"time"
 )
 
 // create a new type of 'deck'
@@ -63,8 +64,12 @@ func newDeckFromFile(filename string) deck {
 
 //suffle data
 func (d deck) shuffle() {
+	//generator random number
+	source:= rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
