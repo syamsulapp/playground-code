d362db1e39f6287fc37aab9663c841cbc3ff084a
samsul <samsul.marif100@gmail.com>
2024-08-17 16:21:11 +0800

    channel and go routine
    - implement check link
    - use function literal(anonymous function)

--- Golang/udemy/channels.go
+++ Golang/udemy/channels.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func ListUrl() {
@@ -14,16 +15,34 @@ func ListUrl() {
 		"https://dev-simfoni.idijakpus.or.id",
 	}
 
+	c := make(chan string)
 	for _, link := range links {
-		CheckLink(link)
+		go CheckLink(link, c) // assigned to (go_routine)
+	}
+
+	// for i := 0; i < len(links); {
+	// 	fmt.Println(<-c) //excec channel base on length of data links
+	// }
+
+	//repeating routes
+	for l := range c {
+		// time.Sleep(5 * time.Second)
+		// go CheckLink(l, c)
+
+		go func(link string) { // func literal(function anonymous)
+			time.Sleep(5 * time.Second)
+			CheckLink(link, c)
+		}(l)
 	}
 }
 
-func CheckLink(link string) {
+func CheckLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "maybe site is down")
+		c <- "Migth be down url" //channel syntax excec of go_routine
 	}
 
 	fmt.Println(link, "is up")
+	c <- "yes link is up"
 }
