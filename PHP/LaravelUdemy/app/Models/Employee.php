758f6e4c10c673963c3772af39ff9956f1f8f7a1
samsul <samsul.marif100@gmail.com>
2024-08-19 11:22:33 +0800

    create model for employee

---
+++ PHP/LaravelUdemy/app/Models/Employee.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = ['name', 'email', 'phone_number', 'age', 'gender'];
+
+    use HasFactory;
+}
