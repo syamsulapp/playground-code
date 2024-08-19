c1a78ba85bca7cdc92cdd6cdb9fde9fcffd6358d
samsul <samsul.marif100@gmail.com>
2024-08-19 23:23:56 +0800

    migrations-table
    - users update
    - new courses by users

--- PHP/LaravelUdemy/database/migrations/2024_08_19_151944_create_courses_table.php
+++ PHP/LaravelUdemy/database/migrations/2024_08_19_151944_create_courses_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone_no', 50);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('users_id')->references('id')->on('users');
+            $table->string('title', 100);
+            $table->text('description');
+            $table->integer('total_videos');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courses');
     }
 };
