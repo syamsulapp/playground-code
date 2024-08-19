14c08761b1c4274369e093accf933c20b7a00bf5
samsul <samsul.marif100@gmail.com>
2024-08-19 11:15:34 +0800

    create controller for employee

---
+++ PHP/LaravelUdemy/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //add employee
+    public function AddEmployee() {}
+
+    //list employee
+    public function ListEmployee() {}
+
+    //detail detail
+    public function DetailEmployee($EmployeeID) {}
+
+    //update detail
+    public function UpdateEmployee($EmployeeID) {}
+
+    //delete detail
+    public function DeleteEmployee($EmployeeID) {}
+}
