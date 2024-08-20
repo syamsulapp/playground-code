caf9dade2f04625db4e52900f45210a5eb7f9c8e
samsul <samsul.marif100@gmail.com>
2024-08-20 19:09:32 +0800

    controllers
    - users
    - course

---
+++ PHP/LaravelUdemy/app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function CourseEnroll(Request $request) {}
+
+    public function ListCourse() {}
+
+    public function DeleteCourse($DeleteCourseID) {}
+}
---
+++ PHP/LaravelUdemy/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function Login() {}
+
+    public function Register() {}
+
+    public function Logout() {}
+
+    public function Profile() {}
+}
