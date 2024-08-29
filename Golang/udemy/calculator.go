62dbe26daa13acbf902ffddda6b1b5ababec9008
syamsulapp <samsul.marif100@gmail.com>
2024-08-30 06:00:44 +0800

    calculator project
    - create interface of handler every func calucator

--- Golang/udemy/calculator.go
+++ Golang/udemy/calculator.go
@@ -1,4 +1,35 @@
 package main
 
+import "fmt"
+
 type TypeCalculator interface {
+	Penjumlahan() int
+	Pembagian() int
+	Perkalian() int
+	Pengurangan() int
+}
+
+type InputOfTypeCalculator struct {
+	x int
+	y int
+}
+
+func (HandleInput InputOfTypeCalculator) Penjumlahan() int {
+	return HandleInput.x + HandleInput.y
+}
+
+func (HandleInput InputOfTypeCalculator) Pengurangan() int {
+	return HandleInput.x - HandleInput.y
+}
+
+func (HandleInput InputOfTypeCalculator) Perkalian() int {
+	return HandleInput.x * HandleInput.y
+}
+
+func (HandleInput InputOfTypeCalculator) Pembagian() int {
+	return HandleInput.x / HandleInput.y
+}
+
+func PrintPenjumlahan(HandleTypeCalculator TypeCalculator) {
+	fmt.Println(HandleTypeCalculator.Penjumlahan())
 }
